a4f606de1450863b64258c7afd270caab3f8917c
srilokbodla <srilokbodla@gmail.com>
2017-06-28 01:05:52 +0530

    added filters to get key words

--- summary.py
+++ summary.py
@@ -5,43 +5,53 @@ import nltk
 from sklearn.feature_extraction.text import TfidfVectorizer
 from nltk.corpus import stopwords
 
-with open("reviews1.json") as json_data:
-    data_file = json.load(json_data)
+with open("reviews.json") as json_data:
+    data_file= json.load(json_data)
 
 data = []
 
 for i in range(len(data_file)):
-    data.append(data_file[i]['reviewText'])
+   data.append(data_file[i]['reviewText'])
 
-print data
-
+#print data
 
 sentence = []
 for i in data:
     sentence.append((nltk.sent_tokenize(i)))
 
-print sentence
+#print sentence
 
 stop = set(stopwords.words('english') + list(string.punctuation))
 
-word_list=[]
+word_list = []
 for i in data:
     word_list.append((nltk.word_tokenize(i)))
 
-useful_words=[]
+useful_words = []
 
 for i in word_list:
-    word=[]
+    word = []
     for item in i:
         if item not in stop:
             word.append(item)
-    useful_words.append(string.join(word , " "))
+    useful_words.append(string.join(word, " "))
 
-print useful_words[0][0]
+# print useful_words[0][0]
 
-vector = TfidfVectorizer(max_features=30 , ngram_range=(2,3))
-x= vector.fit_transform(useful_words)
+vector = TfidfVectorizer(max_features=30, ngram_range=(2, 3))
+x = vector.fit_transform(useful_words)
 
 ngrams = vector.get_feature_names()
 
-print ngrams
+keys = []
+
+keywords = []
+for i in range(len(ngrams)):
+    keys.append(nltk.pos_tag(nltk.word_tokenize(ngrams[i])))
+    if len(set([keys[i][0][1] , keys[i][1][1]]).intersection(['JJ' , 'JJR' , 'JJS' , 'RB' , 'RB' , 'RBR' , 'RBS'])):
+        keywords.append(ngrams[i])
+
+#print keys
+
+
+print keywords
