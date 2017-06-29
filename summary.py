817e0cf41c0bc97158462c6026ab8a83868dc1d1
srilokbodla <srilokbodla@gmail.com>
2017-06-30 00:38:03 +0530

    reviews are extracted

--- summary.py
+++ summary.py
@@ -6,28 +6,26 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 from nltk.corpus import stopwords
 
 with open("reviews.json") as json_data:
-    data_file= json.load(json_data)
+    data_file = json.load(json_data)
 
 data = []
 
 for i in range(len(data_file)):
-   data.append(data_file[i]['reviewText'])
+    data.append(data_file[i]['reviewText'])
 
-#print data
+# print data
 
-sentence = []
-for i in data:
-    sentence.append((nltk.sent_tokenize(i)))
-
-#print sentence
-
+# making list of not useful word
 stop = set(stopwords.words('english') + list(string.punctuation))
 
+# tokenizing review text into word
 word_list = []
 for i in data:
     word_list.append((nltk.word_tokenize(i)))
 
 useful_words = []
+
+# removing stopwords
 
 for i in word_list:
     word = []
@@ -37,6 +35,8 @@ for i in word_list:
     useful_words.append(string.join(word, " "))
 
 # print useful_words[0][0]
+# tfidfvector is used to assign tfidf value to tokens
+# token size can be regulated using ngram_range by default it is 1
 
 vector = TfidfVectorizer(max_features=30, ngram_range=(2, 3))
 x = vector.fit_transform(useful_words)
@@ -46,12 +46,36 @@ ngrams = vector.get_feature_names()
 keys = []
 
 keywords = []
+# extracting key words by pos tagging
 for i in range(len(ngrams)):
     keys.append(nltk.pos_tag(nltk.word_tokenize(ngrams[i])))
-    if len(set([keys[i][0][1] , keys[i][1][1]]).intersection(['JJ' , 'JJR' , 'JJS' , 'RB' , 'RB' , 'RBR' , 'RBS'])):
+    if len(set([keys[i][0][1], keys[i][1][1]]).intersection(['JJ', 'JJR', 'JJS', 'RB', 'RB', 'RBR', 'RBS'])):
         keywords.append(ngrams[i])
 
-#print keys
+# print keys
 
+# filtering extracted keywords
+#print keywords
+a = []
+for i in range(len(keywords)):
+    a.append(nltk.pos_tag(nltk.word_tokenize(keywords[i])))
+    if len(set([a[i][0][1], a[i][1][1]]).intersection(['PRP', 'IN', 'DT', 'FW', 'LS', 'MD', 'POS', 'SYM', 'TO', 'CD', 'CC', 'UH'])):
+        del keywords[i]
 
-print keywords
+key1 = []
+key2 = []
+key3 = []
+
+# making list of reviews containing keywords (for each keyword separate list)
+
+for line in range(len(useful_words)):
+    if useful_words[line].find(keywords[0]) != -1:
+        key1.append(data_file[line]['reviewText'])
+    elif useful_words[line].find(keywords[1]) != -1:
+        key2.append(data_file[line]['reviewText'])
+    elif useful_words[line].find(keywords[7]) != -1:
+        key3.append(data_file[line]['reviewText'])
+
+print len(key1)
+print len(key2)
+print len(key3)
